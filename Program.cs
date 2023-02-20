c2bc01cbb8eea35353aec7aeaefabb6500a82c5b
yusufmetin81 <yusufmetin262@gmail.com>
2023-02-20 22:53:29 +0300

    Console Uygulaması
    Girilen iki ürün fiyatının toplamı 200'den fazla olması durumunda 2. Ürüne %25 indirim uygulayan program.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2._ürüne_indirim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double urun1, urun2, toplam;
+            
+            Console.Write("Birinci Ürünün Fiyatını Giriniz= ");
+            urun1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("İkinci Ürünün Fiyatını Giriniz= ");
+            urun2 = Convert.ToDouble(Console.ReadLine());
+
+            toplam = urun1 + urun2;
+
+            if (toplam >= 200)
+            {
+                toplam -= urun2 * 0.25;
+                Console.Write("İndirimli Toplam Fiyat = {0} TL", toplam);
+            }
+            else
+            {
+                Console.Write("Normal Toplam Fiyat = {0} TL", toplam);
+            }
+            Console.ReadKey();
+        }
+    }
+}
